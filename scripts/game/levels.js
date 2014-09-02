6739dc078244743d4052f334187c0f58730b4f52
Lucas Vieira <newlucasfevi@gmail.com>
2014-09-02 11:35:53 -0300

    Added the methods and properties needed for now

--- scripts/game/levels.js
+++ scripts/game/levels.js
@@ -1,3 +1,33 @@
 var levels = {
-    
+
+    enemies: null,
+    initialGold: null,
+    initialLives: null,
+    currentWave: null,
+    state: 'waiting',
+    data: {},
+
+    init: function() {
+
+    },
+
+    draw: function() {
+
+    },
+
+    update: function() {
+
+    },
+
+    start: function() {
+
+    },
+
+    stop: function() {
+
+    },
+
+    nextWave: function() {
+
+    },
 }
