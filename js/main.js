4e7cf022bc45bd839e76132f65cd5a7279caecaf
Lucas Vieira <newlucasfevi@gmail.com>
2014-09-04 20:01:05 -0300

    Started to use the Phaser framework. No longed need the PreloadJS. Added the .gitignore. Changed the files structure.

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'canvas', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+  game.stage.backgroundColor = '#5aac7b';
+
+  // Listening these events from Phaser.Loader
+  game.load.onLoadStart.add(loadStart, this);
+  game.load.onFileComplete.add(fileComplete, this);
+  game.load.onLoadComplete.add(loadComplete, this);
+
+  game.load.image('big', 'assets/big.jpg');
+  game.load.image('enemies', 'assets/enemies.png');
+  game.load.image('background', 'assets/background.png');
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
+
+function render() {
+
+}
+
+// Loader callbacks
+function loadStart() {
+
+}
+
+function fileComplete(progress, cacheKey, success, totalLoaded, totalFiles) {
+
+  $('#progressValue span').html(progress);
+  $('#progressBar').val(progress);
+
+}
+
+function loadComplete() {
+
+}
