c371ce1e0ea6befe00c6419df833487d22470abd
Lucas Vieira <newlucasfevi@gmail.com>
2014-09-09 10:22:54 -0300

    Add the new Levels class and removed some divs

---
+++ js/Levels.js
@@ -0,0 +1,29 @@
+Levels = function(game) {
+
+    this.game = game;
+
+};
+
+Levels.prototype = {
+
+    preload: function() {
+
+        this.game.load.atlasXML('RPGPack', 'assets/sprites/RPGPackSheet.png', 'assets/sprites/RPGPackSheet.xml');
+
+    },
+
+    create: function() {
+
+    },
+
+    update: function() {
+
+    },
+
+    start: function() {
+
+
+
+    }
+
+};
