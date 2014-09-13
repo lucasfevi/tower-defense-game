52cd013385ae84ab6ef6b87f82f1713850437181
Lucas Vieira <newlucasfevi@gmail.com>
2014-09-13 16:56:03 -0300

    Now the Enemy class extends from the Phaser.Sprite

--- js/class/Enemy.js
+++ js/class/Enemy.js
@@ -3,30 +3,27 @@ Enemy = function(game, path, data) {
     this.game = game;
     this.path = path;
 
+    this.config(data);
+};
+
+Enemy.prototype = Object.create(Phaser.Sprite.prototype);
+Enemy.prototype.constructor = Enemy;
+
+Enemy.prototype.update = function() {
+
+    // console.log(this.alive);
+};
+
+Enemy.prototype.config = function(data) {
+
+    // Fix the -30 hack on the y position later
+    Phaser.Sprite.call(this, this.game, this.path[0].x * 32, this.path[0].y * 32 - 30, 'Enemies', data.frameName);
+
     this.name = data.name;
     this.hp = data.hp;
     this.speed = data.speed;
     this.power = data.power;
     this.value = data.value;
-    this.frameName = data.frameName;
-
-    this.config();
-};
-
-Enemy.prototype.update = function() {
-
-
-};
-
-Enemy.prototype.config = function() {
-
-    // Don't add it to the game here???
-    // Also, fix the -30 hack on the y position later
-    this.sprite = this.game.add.sprite(this.path[0].x * 32, this.path[0].y * 32 - 30, 'Enemies', this.frameName);
-
-    // Physics
-    this.game.physics.arcade.enable(this.sprite);
-    this.sprite.body.velocity.setTo(0, 0);
 
     this.tween = null;
     this.setTween();
@@ -34,14 +31,16 @@ Enemy.prototype.config = function() {
 
 Enemy.prototype.setTween = function() {
 
-    var duration = (this.game.physics.arcade.distanceToXY(this.sprite, this.path[0].x * 32, this.path[0].y * 32) / this.speed) * 1000;
+    var duration = (this.game.physics.arcade.distanceToXY(this, this.path[0].x * 32, this.path[0].y * 32) / this.speed) * 1000;
 
-    this.tween = this.game.add.tween(this.sprite);
+    this.tween = this.game.add.tween(this);
 
     for (var i = 0; i < this.path.length; i++)
     {
         this.tween.to({ x: this.path[i].x * 32, y: this.path[i].y * 32 }, duration, Phaser.Easing.Linear.None);
     }
+
+    this.tween.to({ x: this.path[this.path.length - 1].x * 32, y: this.path[this.path.length - 1].y * 32 - 30 }, duration, Phaser.Easing.Linear.None);
 
     this.tween.start();
 }
